bd02988792e323fe38acdaf65ee69d915c948a80
avinash23em <ap23vi@gmail.com>
2025-07-25 23:10:47 +0530

    intial

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/student/home.jsx'
+
+function App() {
+  
+
+  return (
+   <div>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+  </Routes>
+   </div>
+      
+  )
+}
+
+export default App
---
+++ client/src/pages/student/home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+return (
+    <div>
+        <h1>Hoem page</h1>
+    </div>
+)
+
+}
+
+export default Home;
